ad2eaf062f125c3d4a6b6f39d3e700c6d90e18e9
Gabriel Villarroel <gabriel776977@gmail.com>
2023-02-25 22:06:02 -0300

    fixing some things

--- real-estate-web-application/back/src/api/GeneralUsers/auth/strategies/google.strategy.ts
+++ real-estate-web-application/back/src/api/GeneralUsers/auth/strategies/google.strategy.ts
@@ -8,12 +8,21 @@ config();
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 	constructor() {
-		super({
-			clientID: "256044242886-oibj8tr4oa8st4i2jbqvggjrn9l56ol1.apps.googleusercontent.com",
-			clientSecret: "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",
-			callbackURL: "http://localhost:3001/clients/login/google/callback",
+		super(
+		// 	{
+		// 	clientID: "256044242886-oibj8tr4oa8st4i2jbqvggjrn9l56ol1.apps.googleusercontent.com",
+		// 	clientSecret: "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",
+		// 	callbackURL: "http://localhost:3001/clients/login/google/callback",
+		// 	scope: ['email', 'profile'],
+		// }
+		{
+			clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+			clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+			callbackURL: `${process.env.GOOGLE_CLIENT_CALLBACK_URL}`,
 			scope: ['email', 'profile'],
-		});
+		}
+		
+		);
 	}
 
 	async validate(
